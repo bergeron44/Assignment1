8cb91b7cf6b65721b754c63678ab622d24688fb9
asafccoh <asaf8071@gmail.com>
2024-05-09 18:06:12 +0300

    Update Beneficiary.cpp

---
+++ Skeleton/src/Beneficiary.cpp
@@ -0,0 +1,37 @@
+#include "../include/Beneficiary.h"
+
+Beneficiary::Beneficiary(int id, const string &name, int locationDistance, int maxRequests)
+    : id(id), name(name), locationDistance(locationDistance), maxRequests(maxRequests) {}
+
+const string &Beneficiary::getName() const {
+    return name;
+}
+
+int Beneficiary::getId() const {
+    return id;
+}
+
+int Beneficiary::getBeneficiaryDistance() const {
+    return locationDistance;
+}
+
+int Beneficiary::getMaxRequests() const {
+    return maxRequests;
+}
+
+int Beneficiary::getNumRequests() const {
+    return requestsId.size();
+}
+
+bool Beneficiary::canMakeRequest() const {
+    return requestsId.size() < maxRequests;
+}
+
+const vector<int> &Beneficiary::getRequestsIds() const {
+    return requestsId;
+}
+
+int Beneficiary::addRequest(int RequestId) {
+    requestsId.push_back(RequestId);
+    return RequestId; // Return RequestId if added successfully
+}
