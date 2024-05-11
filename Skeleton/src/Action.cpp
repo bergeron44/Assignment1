e12badf41749fdc1c9192eb5000677a0fe968ee5
ron berger <ronberger40@gmail.com>
2024-05-11 17:08:30 +0000

    ron action

--- Skeleton/src/Action.cpp
+++ Skeleton/src/Action.cpp
@@ -22,14 +22,38 @@ string CoreAction::getErrorMsg() const {
 }
 
 // Implementation of SimulateStep class
-SimulateStep::SimulateStep(int numOfSteps) : numOfSteps(numOfSteps) {}
+SimulateStep::SimulateStep(int numOfSteps) : CoreAction(), numOfSteps(numOfSteps) {}
 
 void SimulateStep::act(MedicalWareHouse &medWareHouse) {
-    // Implementation of act method
+       medWareHouse.addAction(this);
+
+    if (numOfSteps <= 0)
+    {
+        error("Number of steps must be positive!");
+        return;
+    }
+
+    for (int i = 0; i < numOfSteps; i++)
+        medWareHouse.simulateStep();
+
+    complete();
 }
 
 std::string SimulateStep::toString() const {
-    // Implementation of toString method
+     string name = "SimulateStep";
+    string args = std::to_string(numOfSteps);
+    string s ;
+      if (this->getStatus() == ActionStatus::ERROR)
+      {
+        s= "ERROR";
+      }
+
+      else
+      {
+        s="COMPLETED";
+      }
+      
+    return name + " " + args + " " + s;
 }
 
 SimulateStep* SimulateStep::clone() const {
@@ -37,17 +61,61 @@ SimulateStep* SimulateStep::clone() const {
 }
 
 // Implementation of AddRequset class
-AddRequset::AddRequset(int id) : beneficiaryId(id) {}
+AddRequset::AddRequset(int id) : CoreAction(), beneficiaryId(id) {}
 
-void AddRequset::act(MedicalWareHouse &medWareHouse) {
-    // Implementation of act method
+void AddRequset::act(MedicalWareHouse &medWareHouse)
+{
+    medWareHouse.addAction(this);
+
+    if (beneficiaryId < 0)
+    {
+        error("Beneficiary ID must be positive");
+        return;
+    }
+
+    Beneficiary &Beneficiary = medWareHouse.getBeneficiary(beneficiaryId);
+    if (Beneficiary.getId() == -1)
+    {
+        error("Customer does not exist");
+        return;
+    }
+
+    if (!Beneficiary.canMakeRequest())
+    {
+        error("Beneficiary has reached max SupplyRequest amount");
+        return;
+    }
+
+    SupplyRequest *SupplyRequest = new SupplyRequest(medWareHouse.getNewSupplyRequestId(), customerId, customer.getCustomerDistance());
+
+    medWareHouse.SupplyRequest(SupplyRequest);
+    if (customer.SupplyRequest(SupplyRequest->getId()) == -1)
+    {
+        error("FATAL @ AddOrder::Act");
+        return;
+    }
+    complete();
 }
 
-string AddRequset::toString() const {
-    // Implementation of toString method
+string AddRequset::toString() const
+{
+    string name = "AddOrder";
+    string args = std::to_string(beneficiaryId);
+    string s ;
+      if (this->getStatus() == ActionStatus::ERROR)
+      {
+        s= "ERROR";
+      }
+
+      else
+      {
+        s="COMPLETED";
+      }
+    return name + " " + args + " " + s;
 }
 
-AddRequset* AddRequset::clone() const {
+AddRequset *AddRequset::clone() const
+{
     return new AddRequset(*this);
 }
 
